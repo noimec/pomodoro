0178e722e1ff4fff26f72cc04706067c01eb96e3
ingerstep <stepan.karelin.1996@mail.ru>
2025-03-12 06:14:06 +0700

    POMO-13 update button UI props

--- src/features/timer/ui/timer-buttons.tsx
+++ src/features/timer/ui/timer-buttons.tsx
@@ -1,34 +1,53 @@
 import { Button } from '@/shared/ui/button';
 import { useCountdown } from '../lib/use-countdown';
 import { getTimerData } from '../lib/get-timer-data';
+import { getTasksArray } from '@/entities/task';
 
 export const TimerButtons = () => {
   const { pause, skip, start, resume } = useCountdown();
   const { isRunning, isPaused } = getTimerData();
-
+  const taskIsEmpty = getTasksArray();
   return (
     <div>
       {isRunning ? (
         <>
-          <Button size='sm' variant='green' onClick={pause} className='mr-6'>
+          <Button
+            disabled={!taskIsEmpty}
+            size='sm'
+            variant='green'
+            onClick={pause}
+            className='mr-6'
+          >
             Пауза
           </Button>
-          <Button onClick={skip} size='md' variant='red'>
+          <Button disabled={!taskIsEmpty} onClick={skip} size='md' variant='red'>
             Пропустить
           </Button>
         </>
       ) : (
         <>
           {!isPaused ? (
-            <Button size='sm' variant='green' onClick={start} className='mr-6'>
+            <Button
+              disabled={!taskIsEmpty}
+              size='sm'
+              variant='green'
+              onClick={start}
+              className='mr-6'
+            >
               Старт
             </Button>
           ) : (
-            <Button size='md' variant='green' onClick={resume} className='mr-6'>
+            <Button
+              disabled={!taskIsEmpty}
+              size='md'
+              variant='green'
+              onClick={resume}
+              className='mr-6'
+            >
               Продолжить
             </Button>
           )}
-          <Button size='sm' variant='red' onClick={skip}>
+          <Button disabled={!taskIsEmpty} size='sm' variant='red' onClick={skip}>
             Стоп
           </Button>
         </>
