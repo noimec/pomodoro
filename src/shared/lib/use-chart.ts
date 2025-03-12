6e41fd21f61704eb78adbfc06e673248c928ab1b
ingerstep <stepan.karelin.1996@mail.ru>
2025-03-12 14:04:23 +0700

    POMO-21 feat: init mongodb docker & prisma orm

--- src/shared/lib/use-chart.ts
+++ src/shared/lib/use-chart.ts
@@ -1,79 +1,79 @@
-// import { useState } from 'react';
-// import {
-//   Chart as ChartJS,
-//   CategoryScale,
-//   LinearScale,
-//   PointElement,
-//   LineElement,
-//   Title,
-//   Tooltip,
-//   Legend,
-//   ChartData,
-//   ChartOptions,
-// } from 'chart.js';
+import { useState } from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  ChartData,
+  ChartOptions,
+} from 'chart.js';
 
-// ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
-// export const useChart = () => {
-//   const [filteredData, setFilteredData] = useState(STATISTICS_DATA);
+export const useChart = () => {
+  const [filteredData, setFilteredData] = useState();
 
-//   const chartData: ChartData<'line'> = {
-//     labels: filteredData.map((data) => getDayOfWeek(data.date)),
-//     datasets: [
-//       {
-//         label: 'Фокус Время',
-//         data: filteredData.map((data) => data.value),
-//         borderColor: '#B7280F',
-//         backgroundColor: '#FFD200',
-//         tension: 0.1,
-//       },
-//     ],
-//   };
+  const chartData: ChartData<'line'> = {
+    labels: filteredData.map((data) => getDayOfWeek(data.date)),
+    datasets: [
+      {
+        label: 'Фокус Время',
+        data: filteredData.map((data) => data.value),
+        borderColor: '#B7280F',
+        backgroundColor: '#FFD200',
+        tension: 0.1,
+      },
+    ],
+  };
 
-//   const chartOptions: ChartOptions<'line'> = {
-//     responsive: true,
-//     plugins: {
-//       legend: {
-//         position: 'top',
-//         labels: {
-//           color: '#3498DB',
-//         },
-//       },
-//       tooltip: {
-//         mode: 'index',
-//         intersect: false,
-//       },
-//     },
-//     scales: {
-//       x: {
-//         type: 'category',
-//         title: {
-//           display: true,
-//           text: 'День недели',
-//           color: '#3498DB',
-//         },
-//         ticks: {
-//           color: '#3498DB',
-//         },
-//       },
-//       y: {
-//         type: 'linear',
-//         title: {
-//           display: true,
-//           text: 'Время (минуты)',
-//           color: '#3498DB',
-//         },
-//         ticks: {
-//           color: '#3498DB',
-//         },
-//         beginAtZero: true,
-//       },
-//     },
-//   };
+  const chartOptions: ChartOptions<'line'> = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+        labels: {
+          color: '#3498DB',
+        },
+      },
+      tooltip: {
+        mode: 'index',
+        intersect: false,
+      },
+    },
+    scales: {
+      x: {
+        type: 'category',
+        title: {
+          display: true,
+          text: 'День недели',
+          color: '#3498DB',
+        },
+        ticks: {
+          color: '#3498DB',
+        },
+      },
+      y: {
+        type: 'linear',
+        title: {
+          display: true,
+          text: 'Время (минуты)',
+          color: '#3498DB',
+        },
+        ticks: {
+          color: '#3498DB',
+        },
+        beginAtZero: true,
+      },
+    },
+  };
 
-//   return {
-//     chartData,
-//     chartOptions,
-//     setFilteredData,
-//   };
-// };
+  return {
+    chartData,
+    chartOptions,
+    setFilteredData,
+  };
+};
